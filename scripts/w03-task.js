898cc7603daf0847675ba3978f159a764a7720c9
Daniel Cross <danielcross2872@gmail.com>
2023-09-30 16:28:30 -0600

    9_30

---
+++ scripts/w03-task.js
@@ -0,0 +1,41 @@
+/* LESSON 3 - Programming Tasks */
+/* Author - Daniel Cross */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+function add(number1, number2){
+    return number1 + number2
+}
+
+function addNumbers(){
+    addNumber1 = Number(document.querySelector('#add1').value);
+    addNumber2 = Number(document.querySelector('#add2').value);
+    document.querySelector('#sum').value = add(addNumber1, addNumber2);
+}
+
+document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+
+/* Function Expression - Subtract Numbers */
+
+
+/* Arrow Function - Multiply Numbers */
+
+
+/* Open Function Use - Divide Numbers */
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
