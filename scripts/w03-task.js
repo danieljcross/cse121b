b1d1b38859f97e21380fa7ed77aa04d0060f4dea
Daniel Cross <danielcross2872@gmail.com>
2023-10-03 01:39:43 -0600

    update_fix

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -3,8 +3,9 @@
 
 /* FUNCTIONS */
 /* Function Definition - Add Numbers */
+
 function add(number1, number2){
-    return number1 + number2
+    return number1 + number2;
 }
 
 function addNumbers(){
@@ -17,25 +18,72 @@ document.querySelector('#addNumbers').addEventListener('click', addNumbers);
 
 /* Function Expression - Subtract Numbers */
 
+function subtract(number1, number2){
+    return number1 - number2;
+}
+
+function subtractNumbers(){
+    subtractNumber1 = Number(document.querySelector('#subtract1').value);
+    subtractNumber2 = Number(document.querySelector('#subtract2').value);
+    document.querySelector('#difference').value = subtract(subtractNumber1, subtractNumber2);
+}
+
+document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
 
 /* Arrow Function - Multiply Numbers */
 
+function multiply(number1, number2){
+    return number1 * number2;
+}
+
+function multiplyNumbers(){
+    factor1 = Number(document.querySelector('#factor1').value);
+    factor2 = Number(document.querySelector('#factor2').value);
+    document.querySelector('#product').value = multiply(factor1, factor2);
+}
+
+document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
 
 /* Open Function Use - Divide Numbers */
 
+function divide(number1, number2){
+    return number1 / number2;
+}
 
-/* Decision Structure */
+function divideNumbers(){
+    dividend = Number(document.querySelector('#dividend').value);
+    divisor = Number(document.querySelector('#divisor').value);
+    document.querySelector('#quotient').value = divide(dividend, divisor);
+}
+
+document.querySelector('#divideNumbers').addEventListener('click', divideNumbers);
+
 
 
 /* ARRAY METHODS - Functional Programming */
 /* Output Source Array */
 
+let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+document.querySelector('#array').innerHTML = numbersArray;
+
 /* Output Odds Only Array */
+
+let odds = numbersArray.filter(number => number % 2 === 1);
+document.querySelector('#odds').innerHTML = odds;
 
 /* Output Evens Only Array */
 
+document.querySelector('#evens').innerHTML = numbersArray.filter(number => number % 2 === 0);
+
 /* Output Sum of Org. Array */
+
+document.querySelector('#sumOfArray').innerHTML = numbersArray.reduce((sum, number) => sum + number);
 
 /* Output Multiplied by 2 Array */
 
+document.querySelector('#multiplied').innerHTML = numbersArray.map(number => number * 2);
+
 /* Output Sum of Multiplied by 2 Array */
+
+let x = numbersArray.map(number => number * 2)
+document.querySelector('#sumOfMultiplied').innerHTML = x.reduce((sum, number) => sum + number);
