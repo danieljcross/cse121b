9841848dab3669f4519e6c999c00473ad15fe145
Daniel Cross <danielcross2872@gmail.com>
2023-10-18 17:36:11 -0600

    w3_fixes

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -11,9 +11,10 @@ const profilePicture = 'images/personal_picture.jpeg';
 /* Step 3 - Element Variables */
 
 const nameElement = document.getElementById('name');
-const foodElement = document.getElementById('food');
+let foodElement = document.getElementById('food');
 const yearElement = document.getElementById('year');
-const imageElement = document.getElementById('profilePicture');
+const imageElement = document.querySelector('img');
+imageElement.src = profilePicture;
 
 
 /* Step 4 - Adding Content */
@@ -23,15 +24,16 @@ yearElement.textContent = `${currentYear}`;
 imageElement.setAttribute('src', profilePicture);
 imageElement.setAttribute('alt', 'Profile image of ', fullName);
 
-
-
 /* Step 5 - Array */
 
 let favoriteFoods = ['Cheeseburgers', 'Pizza', 'Tacos', 'Pasta'];
-foodElement.innerHTML = favoriteFoods;
+foodElement.textContent = favoriteFoods;
+
 let favoriteFoodItem = 'Corn Dogs';
 favoriteFoods.push(favoriteFoodItem);
+
 foodElement.innerHTML += `<br>${favoriteFoods}`;
 favoriteFoods.splice(0, 1);
+foodElement.innerHTML += `<br>${favoriteFoods}`;
 favoriteFoods.pop();
 foodElement.innerHTML += `<br>${favoriteFoods}`;
